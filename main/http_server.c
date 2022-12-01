92746757b0f8a492391ccc3c275acbc97a61ab50
Mykola Luniv <lunivm@yahoo.com>
2022-12-01 21:51:28 +0200

    add http server support for hdt22

--- main/http_server.c
+++ main/http_server.c
@@ -7,6 +7,7 @@
 #include "http_server.h"
 #include "tasks_common.h"
 #include "wifi_app.h"
+#include "DHT22.h"
 
 // Tag used for ESP serial console messages
 static const char TAG[] = "http_server";
@@ -210,6 +211,25 @@ static esp_err_t http_server_favicon_ico_handler(httpd_req_t *req)
 
 	httpd_resp_set_type(req, "image/x-icon");
 	httpd_resp_send(req, (const char *)favicon_ico_start, favicon_ico_end - favicon_ico_start);
+
+	return ESP_OK;
+}
+
+/**
+ * DHT sensor readings JSON handler responds with DHT22 sensor data
+ * @param req HTTP request for which the uri needs to be handled
+ * @return ESP_OK
+ */
+static esp_err_t http_server_get_dht_sensor_readings_json_handler(httpd_req_t *req)
+{
+	ESP_LOGI(TAG, "/dhtSensor.json requested");
+
+	char dhtSensorJSON[100];
+
+	sprintf(dhtSensorJSON, "{\"temp\":\"%.1f\",\"humidity\":\"%.1f\"}", getTemperature(), getHumidity());
+
+	httpd_resp_set_type(req, "application/json");
+	httpd_resp_send(req, dhtSensorJSON, strlen(dhtSensorJSON));
 
 	return ESP_OK;
 }
@@ -436,6 +456,15 @@ static httpd_handle_t http_server_configure(void)
       .user_ctx = NULL
   };
   httpd_register_uri_handler(http_server_handle, &OTA_status);
+
+  // register dhtSensor.json handler
+  httpd_uri_t dht_sensor_json = {
+      .uri = "/dhtSensor.json",
+      .method = HTTP_GET,
+      .handler = http_server_get_dht_sensor_readings_json_handler,
+      .user_ctx = NULL
+  };
+  httpd_register_uri_handler(http_server_handle, &dht_sensor_json);
 
 	return http_server_handle;
 }
